00760a628f8ca2ffecb061fbd69d27e2141c61f6
Ognjen Radisic <59142427+Ognjen-Radisic@users.noreply.github.com>
2020-12-13 20:17:55 +0100

    error page and minor fixes

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Error = () => {
-  return <div></div>
+  return (
+    <section className='section'>
+      Something went wrong, try again later..
+    </section>
+  )
 }
 
 export default Error
